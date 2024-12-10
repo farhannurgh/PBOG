6f2fa8a3d4ad46fdca4910d34feebe961ae074bf
farhannurgh <163228281+farhannurgh@users.noreply.github.com>
2024-12-11 02:26:49 +0700

    Add files via upload

---
+++ Plane/Ticket.java
@@ -0,0 +1,16 @@
+
+public class Ticket {
+    protected double baseFare;
+
+    public Ticket(double baseFare) {
+        this.baseFare = baseFare;
+    }
+
+    public double calculateFare() {
+        return baseFare;
+    }
+
+    public String getFacilities() {
+        return "Fasilitas umum";
+    }
+}
